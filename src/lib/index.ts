73f5dad85dfa36c51bacf9d9d0e3298c571f9067
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-08-31 16:48:00 +0530

    gettng familiar with express

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,9 +5,19 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.get("/users", async (_, res) => {
-  const users = await prisma.user.findFirst();
-  res.json(users);
+// app.get("/users", async (_, res) => {
+//   const users = await prisma.user.findFirst();
+//   res.json(users);
+// });
+
+app.put("/users", async (_, res) => {
+  const updatedUsers = await prisma.user.update({
+    where: { email: "shreyas@gmail.com" },
+    data: { email: "gadhe@gmail.com" },
+  });
+  res.json({
+    updated: updatedUsers,
+  });
 });
 
 app.listen(4000, () => {
